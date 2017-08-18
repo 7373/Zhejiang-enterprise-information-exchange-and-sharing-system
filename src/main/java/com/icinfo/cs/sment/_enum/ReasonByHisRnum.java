b78213156afaeac52f74bea2a76201eb5f28f0e2
anhang <15024454222@qq.com>
2017-08-18 20:34:37 +0800

    v2.0

--- src/main/java/com/icinfo/cs/sment/_enum/ReasonByHisRnum.java
+++
@@ -1,9 +0,0 @@
-package com.icinfo.cs.sment._enum;
-
-public enum ReasonByHisRnum {
-	
-	// 列入
-	INSERT,
-	// 移除
-	REMOVE;
-}
--- src/main/java/com/icinfo/cs/sment/_enum/SmentSourceEnum.java
+++
@@ -1,8 +0,0 @@
-package com.icinfo.cs.sment._enum;
-
-public enum SmentSourceEnum {
-	// 新设 - 新成立的小微企业
-	XS,
-	// 在册 - 通过年报数据列入小微
-	ZC;
-}
