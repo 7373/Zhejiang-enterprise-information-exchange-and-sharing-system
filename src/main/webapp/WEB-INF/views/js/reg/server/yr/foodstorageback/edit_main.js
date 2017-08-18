9b826ef80bbd92b506e0312128e1c6a163296800
anhang <15024454222@qq.com>
2017-08-18 20:40:25 +0800

    v4.0

--- src/main/webapp/WEB-INF/views/js/reg/server/yr/foodstorageback/edit_main.js
+++
@@ -1,3 +0,0 @@
-require(['component/iframeLayer', 'component/dataTable', 'common/util', 'common/http', 'handlebars', 'jquery','jquery.serialize','laydate','jquery.validate'], function (layer, dataTable, util, http, handlebars) {
-
-});
